452362e3c49760b584a3778bc3daa38945f9ee49
Uzair Khan <muhammaduzair751@gmail.com>
2020-08-18 00:48:26 +0500

    install mongodb mongoose bcryptjs faker jsonwebtoken and create setup some repositories

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    uzair:"hello world"
+  });
 });
 
 module.exports = router;
