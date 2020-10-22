078eeb7a8cf00a7170c1c315ee13993e97b86942
Uzair Khan <muhammaduzair751@gmail.com>
2020-10-22 21:31:17 +0500

    added user imformation in dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,13 +1,14 @@
 // in src/App.js
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => 
 (
   <Admin dataProvider={dataProvider} >
-    <Resource name="users"  list={ListGuesser} />
+    <Resource name="users"  list={UserList} />
     </Admin>
     );
 
